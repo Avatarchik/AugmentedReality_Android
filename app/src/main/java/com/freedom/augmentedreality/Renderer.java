6ebe59d8437d76bfcbe34a40825ba1e1b9de32cb
hienbx94 <hienbx94@gmail.com>
2016-03-16 09:49:01 +0700

    init project

---
+++ app/src/main/java/com/freedom/augmentedreality/Renderer.java
@@ -0,0 +1,24 @@
+
+package com.freedom.augmentedreality;
+
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView;
+
+public class Renderer implements GLSurfaceView.Renderer {
+ 
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        MainActivity.nativeSurfaceCreated();
+    }
+
+    public void onSurfaceChanged(GL10 gl, int w, int h) {
+        MainActivity.nativeSurfaceChanged(w, h);
+    }
+
+    public void onDrawFrame(GL10 gl) {
+        MainActivity.nativeDrawFrame();
+    }
+    
+}
---
+++ app/src/main/java/com/freedom/augmentedreality/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.freedom.augmentedreality.app;
+
+/**
+ * Created by hienbx94 on 3/6/16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.216:3000/api/logins";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.216:3000/api/registers";
+}
